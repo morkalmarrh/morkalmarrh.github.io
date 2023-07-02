fe7a87d48890306fc80d01963ace04a7cca5476e
morkalmarrh <hentotheserpent@hotmail.co.uk>
2023-07-02 22:25:30 +0100

    Trying to fix quiz on mobile

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -95,7 +95,6 @@ function showSlide(n) {
   let slideDiv = slides[n];
   let slideAnswers = slideDiv.querySelectorAll('.answers');
   let currentAnswer = slideAnswers[0];
-  quizContainer.innerHTML = currentAnswer.height;
   quizContainer.style.height = currentAnswer.height;
   if(currentSlide === 0){
     previousButton.style.display = 'none';
